41b299ef4ac653223805f42c7d4bd5ec709cf2d7
Bill Huynh <billhuynh700@gmail.com>
2023-03-13 20:38:18 -0700

    about page and home paged update, still need to merge code with fix from ujjwal/ricky

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Slider } from './Slider';
+import { Deliveryinfo } from './Deliveryinfo';
+import { Homeproducts } from './Homeproducts';
+import { Footer } from './Footer';
+
+
 
 export const Home = () => {
   return (
     <div>
          <Slider/>
+         <Deliveryinfo/>
+         <p style={{ margin: '100px' }}></p>
+         <h1 style={{ marginTop: '20px' }}>PREVIEW OUR ITEMS</h1>
+         <Homeproducts/>
+         <Footer/>
+
+         
     </div>
   )
 }
--- src/components/imageData.js
+++ src/components/imageData.js
@@ -26,3 +26,49 @@ export const sliderItems = [
       bg: "fffff",
     },
   ];
+
+
+  export const productItems = [
+    {
+      id: 1,
+      img: chair,
+      title: "#1 CHAIR IN THE MARKET",
+      desc: "GET 10% OFF FIRST TIME PURCHASE",
+      bg: "fFFFF",
+    },
+    {
+      id: 2,
+      img: screw,
+      title: "BUY THIS SCREWDRIVER",
+      desc: "TO SCREW YOUR HEAD STRAIGHT",
+      bg: "fffff",
+    },
+    {
+      id: 3,
+      img: hammer,
+      title: "HAMMER TIME",
+      desc: "GUARANTEE TO LAST A LIFETIME",
+      bg: "fffff",
+    },
+    
+     {
+      id: 4,
+      img: hammer,
+      title: "HAMMER TIME",
+      desc: "GUARANTEE TO LAST A LIFETIME",
+      bg: "fffff",
+    },
+
+     {
+      id: 5,
+      img: hammer,
+      title: "HAMMER TIME",
+      desc: "GUARANTEE TO LAST A LIFETIME",
+      bg: "fffff",
+    },
+
+
+
+    
+  ];
+
