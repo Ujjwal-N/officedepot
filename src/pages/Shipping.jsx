6bacab01f2137a2f5996a27f5c90b09a33951828
Kirsty Lastra <kirsty_lastra@yahoo.com>
2023-04-27 18:39:17 -0700

    sorry

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -29,11 +29,11 @@ export const Shipping = ({ userData }) => {
         <div>
           <h1> Pickup Information </h1>
           <h2> Warehouse 1 </h2>
-          <Map userData={userData} warehouse={'1'} />
+          <Map userData={userData} warehouse={1} />
           <br></br>
           <br></br>
           <h2> Warehouse 2 </h2>
-          <Map userData={userData} warehouse={'2'} />
+          <Map userData={userData} warehouse={2} />
         </div>
       );
     }
